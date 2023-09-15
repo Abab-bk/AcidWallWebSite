f7e0a2429f70818776195f86e9a6c54b3331b2f5
Hey R You Are <1470997723@qq.com>
2023-09-15 15:50:32 +0800

    第一次提交

---
+++ src/utils/bin/commands.ts
@@ -0,0 +1,102 @@
+// List of commands that do not require API calls
+
+import * as bin from './index';
+import config from '../../../config.json';
+
+// Help
+export const help = async (args: string[]): Promise<string> => {
+  const commands = Object.keys(bin).sort().join(', ');
+  var c = '';
+  for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
+    if (i % 7 === 0) {
+      c += Object.keys(bin).sort()[i - 1] + '\n';
+    } else {
+      c += Object.keys(bin).sort()[i - 1] + ' ';
+    }
+  }
+  return `欢迎！以下是所有可用的命令：
+\n${c}\n
+[tab]: 补全命令。
+[ctrl+l]/clear: 清除屏幕\n
+输入 'sumfetch' 来显示我们的信息。
+`;
+};
+
+// About
+export const about = async (args: string[]): Promise<string> => {
+  return `你好，我是 ${config.name}。
+欢迎来到 AcidWallStudio 官方网站！
+更多信息：
+'sumfetch' - 简短介绍.`;
+};
+
+// Contact
+export const email = async (args: string[]): Promise<string> => {
+  window.open(`mailto:${config.email}`);
+  return `Opening mailto:${config.email}...`;
+};
+
+// Search
+export const baidu = async (args: string[]): Promise<string> => {
+  window.open(`https://baidu.com/search?q=${args.join(' ')}`);
+  return `在 Baidu 搜索 ${args.join(' ')}...`;
+};
+
+export const bing = async (args: string[]): Promise<string> => {
+  window.open(`https://bing.com/search?q=${args.join(' ')}`);
+  return `在 Bing 搜索 ${args.join(' ')}?`;
+};
+
+// Typical linux commands
+export const echo = async (args: string[]): Promise<string> => {
+  return args.join(' ');
+};
+
+export const whoami = async (args: string[]): Promise<string> => {
+  return `${config.ps1_username}`;
+};
+
+export const ls = async (args: string[]): Promise<string> => {
+  return `a
+bunch
+of
+fake
+directories`;
+};
+
+export const cd = async (args: string[]): Promise<string> => {
+  return `unfortunately, i cannot afford more directories.
+if you want to help, you can type 'donate'.`;
+};
+
+export const date = async (args: string[]): Promise<string> => {
+  return new Date().toString();
+};
+
+export const clicker = async (args: string[]): Promise<string> => {
+  return `你可以在 <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.games.clicker}" target="_blank">这里</a></u> 找到 炼金术士Clicker`;
+};
+
+export const brain = async (args: string[]): Promise<string> => {
+  return `你可以在 <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.games.brain}" target="_blank">这里</a></u> 找到 三个王国Brain`;
+};
+
+export const buddha = async (args: string[]): Promise<string> => {
+  return `你可以在 <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.games.buddha}" target="_blank">这里</a></u> 找到 如如：功德冲榜模拟器`;
+};
+
+// Banner
+export const banner = (args?: string[]): string => {
+  return `
+  █████╗  ██████╗██╗██████╗ ██╗    ██╗ █████╗ ██╗     ██╗     ███████╗████████╗██╗   ██╗██████╗ ██╗ ██████╗ 
+  ██╔══██╗██╔════╝██║██╔══██╗██║    ██║██╔══██╗██║     ██║     ██╔════╝╚══██╔══╝██║   ██║██╔══██╗██║██╔═══██╗
+  ███████║██║     ██║██║  ██║██║ █╗ ██║███████║██║     ██║     ███████╗   ██║   ██║   ██║██║  ██║██║██║   ██║
+  ██╔══██║██║     ██║██║  ██║██║███╗██║██╔══██║██║     ██║     ╚════██║   ██║   ██║   ██║██║  ██║██║██║   ██║
+  ██║  ██║╚██████╗██║██████╔╝╚███╔███╔╝██║  ██║███████╗███████╗███████║   ██║   ╚██████╔╝██████╔╝██║╚██████╔╝
+  ╚═╝  ╚═╝ ╚═════╝╚═╝╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝ ╚═════╝   
+
+输入 'help' 查看更多命令。
+输入 'sumfetch' 来显示 AcidWallStudio 的信息。
+输入 'repo' 或点击 <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">这里</a></u> 来查看Github仓库。
+`;
+};
---
+++ src/utils/bin/sumfetch.ts
@@ -0,0 +1,43 @@
+import config from '../../../config.json';
+
+const sumfetch = async (args: string[]): Promise<string> => {
+  if (config.ascii === 'cveinnt') {
+    return `                                                  
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          -----------
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓        > A C I D W A L L        ▐▓        <u><a href="https://www.taptap.cn/developer/95033" target="_blank">AcidWallStudio/${config.social.taptap}</a></u>
+▐▓                                 ▐▓       
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓       
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓          
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
+`;
+  } else {
+    return `
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓          -----------
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓        > A C I D W A L L        ▐▓        <u><a href="https://www.taptap.cn/developer/95033" target="_blank">AcidWallStudio/${config.social.taptap}</a></u>
+▐▓                                 ▐▓       
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓       
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓          
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
+`;
+  }
+};
+
+export default sumfetch;
